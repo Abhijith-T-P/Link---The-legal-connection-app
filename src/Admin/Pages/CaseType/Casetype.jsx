a9f323de6fb1bc1e4408d1b89066d831e80097f9
Abhijith <abhijithtp2003@gmail.com>
2023-12-29 22:54:09 +0530

    Admin /Place inner join implimented

--- src/Admin/Pages/CaseType/Casetype.jsx
+++ src/Admin/Pages/CaseType/Casetype.jsx
@@ -15,36 +15,74 @@ import { db } from "../../../config/Firebase";
 import {
   addDoc,
   collection,
-  
+  deleteDoc,
+  doc,
+  getDoc,
   getDocs,
+  updateDoc,
 } from "firebase/firestore";
 import "./Casetype.css";
 
 const Casetype = () => {
   const casetype = collection(db, "CaseType");
   const [caseType, setCaseType] = useState("");
+  const [showCasetype, setShowCasetype] = useState([]);
+  const [selectedid, setSelectedid] = useState(null);
 
   const insCase = async () => {
-    try {
-      await addDoc(casetype, {
+    if (selectedid) {
+      const refUpdate = doc(casetype, selectedid);
+      await updateDoc(refUpdate, {
         CaseType: caseType,
       });
-      alert(`${caseType} inserted `);
+      setSelectedid(null);
+      setCaseType("");
+      getData();
+    } else {
+      try {
+        await addDoc(casetype, {
+          CaseType: caseType,
+        });
+        alert(`${caseType} inserted `);
+        getData();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+  const getData = async () => {
+    try {
+      const data = await getDocs(casetype);
+      const filteredData = data.docs.map((doc, key) => ({
+        ...doc.data(),
+        ID: doc.id,
+      }));
+      console.log(filteredData);
+      setShowCasetype(filteredData);
     } catch (error) {
       console.error(error);
     }
   };
-  const getData = async () => {
-    const data = await getDocs(casetype);
-    const filteredData = data.docs.map((doc, key) => ({
-      ...doc.data(),
-      ID: doc.id,
-    }));
-    console.log(filteredData);
+  const removeData = async (id) => {
+    try {
+      await deleteDoc(doc(casetype, id));
+      getData();
+    } catch (error) {
+      console.error(error);
+    }
   };
+
+  const updateCasetype = async (id) => {
+    const docRef = doc(casetype, id);
+    const docSnap = await getDoc(docRef);
+    const docData = docSnap.data();
+    setSelectedid(id);
+    setCaseType(docData.CaseType);
+  };
+
   useEffect(() => {
     getData();
-  },[]);
+  }, []);
 
   return (
     <div className="Casetype">
@@ -84,20 +122,38 @@ const Casetype = () => {
                     <TableCell align="center">id</TableCell>
                     <TableCell align="center">Casetype</TableCell>
                     <TableCell align="center">Action</TableCell>
+                    <TableCell align="center"></TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  <TableRow>
-                    <TableCell component="th" scope="row" align="center">
-                      id
-                    </TableCell>
-                    <TableCell align="center">CasetypeName </TableCell>
-                    <TableCell align="center">
-                      <Button variant="outlined" type="submit" className="btn">
-                        Delete
-                      </Button>
-                    </TableCell>
-                  </TableRow>
+                  {showCasetype.map((row, key) => (
+                    <TableRow key={key}>
+                      <TableCell component="th" scope="row" align="center">
+                        {key + 1}
+                      </TableCell>
+                      <TableCell align="center">{row.CaseType} </TableCell>
+                      <TableCell align="center">
+                        <Button
+                          variant="outlined"
+                          type="submit"
+                          className="btn"
+                          onClick={() => removeData(row.ID)}
+                        >
+                          Delete
+                        </Button>
+                      </TableCell>
+                      <TableCell>
+                        <Button
+                          variant="outlined"
+                          type="submit"
+                          className="btn"
+                          onClick={() => updateCasetype(row.ID)}
+                        >
+                          Update
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
                 </TableBody>
               </Table>
             </TableContainer>
